c388c69c447cd9bcc6cbc0698de0fe123c2dc627
KSY <ksy91637@naver.com>
2024-11-29 14:08:06 +0900

    test 2.0.1

--- test.py
+++ test.py
@@ -5,13 +5,13 @@ from urllib.parse import quote
 KAKAO_API_KEY = "94725ef93944f848cca35aa808d1deee"
 
 # HTML을 렌더링하기 위한 기본 템플릿
-def generate_map_iframe_html(query):
+def generate_map_iframe_html(query, width, height):
     encoded_query = quote(query)
     return f"""
     <iframe
-        width="800"
-        height="400"
-        src="https://map.kakao.com/?q={encoded_query}"
+        width="{width}"
+        height="{height}"
+        src="https://map.kakao.com/link/search/{encoded_query}"
         frameborder="0"
         allowfullscreen>
     </iframe>
@@ -22,15 +22,19 @@ def main():
     st.title("여행 가이드 챗봇")
     st.write("검색하고자 하는 장소를 입력하세요.")
 
-    user_input = st.text_input("검색어를 입력하세요:")
+    user_input = st.text_input("질문을 입력하세요:")
 
     if user_input:
         if " 지도" in user_input:
             query = user_input.replace(" 지도", "")
-            map_html = generate_map_iframe_html(query)
+            map_html = generate_map_iframe_html(query, 800, 500)
             st.components.v1.html(map_html, height=400)
+        elif " 맛집" in user_input:
+            query = user_input.replace(" 맛집", "")
+            map_html = generate_map_iframe_html(query, 380, 650)
+            st.components.v1.html(map_html, height=650)
         else:
-            st.warning("'지도'를 붙여서 입력해 주세요 (예: '서울 지도').")
+            st.warning("'지도' 또는 '맛집'을 붙여서 입력해 주세요 (예: '서울 지도', '춘천 닭갈비 맛집').")
 
 if __name__ == "__main__":
     main()
